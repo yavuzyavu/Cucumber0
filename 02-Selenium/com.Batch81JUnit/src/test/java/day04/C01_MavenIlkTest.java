af65a9829933e4e444503ae7f5676b16f4ebab0b
Erol Evren <erlevren@gmail.com>
2022-09-15 11:13:26 +0300

    14/09/22

--- 02-Selenium/com.Batch81JUnit/src/test/java/day04/C01_MavenIlkTest.java
+++ 02-Selenium/com.Batch81JUnit/src/test/java/day04/C01_MavenIlkTest.java
@@ -30,9 +30,17 @@ public class C01_MavenIlkTest {
         //3- “Samsung headphones” ile arama yapalim
         aramaKutusu.sendKeys("Samsung headphones", Keys.ENTER);
         //4- Bulunan sonuc sayisini yazdiralim
-        WebElement sonucYazisi = driver.findElement(By.xpath("(//*[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
-        System.out.println(sonucYazisi.getText());
+        //WebElement sonucYazisi = driver.findElement(By.xpath("(//*[@class='a-section a-spacing-small a-spacing-top-small'])[1]"));
+        //System.out.println(sonucYazisi.getText());
+         String [] sonucYazisi = driver.findElement
+                 (By.xpath("(//*[@class='a-section a-spacing-small a-spacing-top-small'])[1]"))
+                 .getText().split(" ");
+        System.out.println("Samsung headphones için arama sonucu = "+sonucYazisi[2]);
         //5- Ilk urunu tiklayalim
+        driver.findElement(By.xpath("(//*[@class='s-image'])[1]")).click();
         //6- Sayfadaki tum basliklari yazdiralim
+        System.out.println("Birinci Başlıklar : "+driver.findElement(By.xpath("//h1")).getText());
+        System.out.println("İkinci Başlıklar : "+driver.findElement(By.xpath("//h2")).getText());
+        System.out.println("Üçüncü Başlıklar : "+driver.findElement(By.xpath("//h3")).getText());
     }
 }
