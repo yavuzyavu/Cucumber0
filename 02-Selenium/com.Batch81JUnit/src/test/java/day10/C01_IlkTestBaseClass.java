5f951bd178b5625f732d1f19aabe7580ce606cf9
Erol Evren <erlevren@gmail.com>
2022-09-21 00:13:29 +0300

    20/09/22

---
+++ 02-Selenium/com.Batch81JUnit/src/test/java/day10/C01_IlkTestBaseClass.java
@@ -0,0 +1,12 @@
+package day10;
+
+import org.junit.Test;
+import utilities.TestBaseBeforeAfter;
+
+public class C01_IlkTestBaseClass extends TestBaseBeforeAfter {
+
+    @Test
+    public void test1(){
+        driver.get("https://amazon.com");
+    }
+}
---
+++ 02-Selenium/com.Batch81JUnit/src/test/java/utilities/TestBaseBeforeAfter.java
@@ -0,0 +1,29 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
+public abstract class TestBaseBeforeAfter {
+    protected WebDriver driver;
+    protected Actions actions;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        actions = new Actions(driver);
+    }
+    @After
+    public void tearDown() {
+        //driver.quit();
+    }
+}
